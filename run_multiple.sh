4ae629683361440f40f8331979ad47180711469f
LasKru <lasse.krueger94@gmail.com>
2021-09-22 14:28:22 +0200

    Updated .gitignore

--- run_multiple.sh
+++ run_multiple.sh
@@ -1,5 +1,8 @@
+
 #script to run the Pipeline multiple times with the specified config files in the "/config_files" directory
 for f in trnl_CH ITS2_config
 do
   bash run_pipeline.sh $PWD/config_files/$f.yaml
 done
+
+#potential addition: track time

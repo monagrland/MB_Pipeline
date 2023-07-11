9066c607fc6b40c6fade429943c456745cfdf6ab
LasKru <lasse.krueger94@gmail.com>
2023-07-11 20:54:22 +0200

    taxonomy results now dont contain empty cells anymore, replaced them with the shortened taxonomic level, i. e. s: for species.

--- scripts/multilvl_taxonomic_classification.py
+++ scripts/multilvl_taxonomic_classification.py
@@ -13,20 +13,47 @@ import argparse
 import re
 import csv
 import sys
+import numpy as np
 
 
 def arg():
     parser = argparse.ArgumentParser("Multilevel direct taxonomic classification")
-    parser.add_argument("-d", "--db_list", nargs="+", default=[], help="List of paths to databases for the direct taxonomic classification.")
+    parser.add_argument(
+        "-d",
+        "--db_list",
+        nargs="+",
+        default=[],
+        help="List of paths to databases for the direct taxonomic classification.",
+    )
     parser.add_argument("-z", "--ASVs", help="Path to ASVs")
-    parser.add_argument("-t", "--threshold", help="Threshold for the direct classification with usearch_global")
-    parser.add_argument("-s", "--sintax_cutoff", help="Sintax Cutoff for the hierarchical classification")
+    parser.add_argument(
+        "-t",
+        "--threshold",
+        help="Threshold for the direct classification with usearch_global",
+    )
+    parser.add_argument(
+        "-s",
+        "--sintax_cutoff",
+        help="Sintax Cutoff for the hierarchical classification",
+    )
     parser.add_argument("-o", "--output", help="Prefered name of the output file")
-    parser.add_argument("-n", "--threads", default=1, help="Number of threads to be used")
-    parser.add_argument("-p", "--hierarchical_db", help="Path for the database for the hierarchical classification")
-    parser.add_argument("-k", "--keep_results", help="Keep Intermediate results of the different levels of taxonomic classification or not. Accepts 'True' and 'False'", type=bool, default=False)
+    parser.add_argument(
+        "-n", "--threads", default=1, help="Number of threads to be used"
+    )
+    parser.add_argument(
+        "-p",
+        "--hierarchical_db",
+        help="Path for the database for the hierarchical classification",
+    )
+    parser.add_argument(
+        "-k",
+        "--keep_results",
+        help="Keep Intermediate results of the different levels of taxonomic classification or not. Accepts 'True' and 'False'",
+        type=bool,
+        default=False,
+    )
     parser.add_argument("-l", "--log", help="Path to Logfile", required=False)
-    return(parser.parse_args())
+    return parser.parse_args()
 
 
 def direct_classification(ASVs, db, threshold, output, threads, run_nr, conda, log):
@@ -60,9 +87,18 @@ def direct_classification(ASVs, db, threshold, output, threads, run_nr, conda, l
     output = f"{os.path.splitext(output)[0]}.{run_nr}.direct.txt"
     output_sam = f"{os.path.splitext(output)[0]}.sam"
     cmd = f"vsearch --usearch_global {ASVs} -db {db} --id {threshold} --uc {output} --threads {threads} -samout {output_sam} -maxaccepts 100 --strand both 2>> {log}"
-    conda_path = subprocess.check_output("conda info | grep 'base environment'", shell=True).decode("utf8").replace("base environment : ", "").replace("(read only)", "").strip()
+    conda_path = (
+        subprocess.check_output("conda info | grep 'base environment'", shell=True)
+        .decode("utf8")
+        .replace("base environment : ", "")
+        .replace("(read only)", "")
+        .strip()
+    )
     conda_act_path = conda_path + "/etc/profile.d/conda.sh"
-    subprocess.run(f". {conda_act_path} && conda activate {conda} && {cmd} && conda deactivate", shell=True)
+    subprocess.run(
+        f". {conda_act_path} && conda activate {conda} && {cmd} && conda deactivate",
+        shell=True,
+    )
     print(f"##### Finished Direct Vsearch Classification level {run_nr} #####")
 
 
@@ -89,7 +125,9 @@ def format_dir_classification(ASVs_path, output, run_nr):
         dataframe containing the classified ASVs and the corresponding taxonomy
 
     """
-    tax_file = pd.read_csv(f"{os.path.splitext(output)[0]}.{run_nr}.direct.txt", sep="\t", header=None)
+    tax_file = pd.read_csv(
+        f"{os.path.splitext(output)[0]}.{run_nr}.direct.txt", sep="\t", header=None
+    )
     nohit_ASV_lst = tax_file.loc[tax_file[9] == "*", 8].tolist()
     ASVs_dict = SeqIO.to_dict(SeqIO.parse(open(ASVs_path), "fasta"))
     fasta_lst = []
@@ -98,7 +136,7 @@ def format_dir_classification(ASVs_path, output, run_nr):
     nohit_fasta_path = f"{os.path.splitext(output)[0]}_nohit_ASVs.fa"
     SeqIO.write(fasta_lst, nohit_fasta_path, "fasta")
     hit_ASVs_df = tax_file.loc[tax_file[9] != "*"]
-    return(nohit_fasta_path, hit_ASVs_df)
+    return (nohit_fasta_path, hit_ASVs_df)
 
 
 def hierarchical_classification(nohit_ASVs, db, output, threads, conda, log):
@@ -127,9 +165,18 @@ def hierarchical_classification(nohit_ASVs, db, output, threads, conda, log):
     print("##### Hierarchical Vsearch Classification #####")
     output = os.path.splitext(output)[0] + ".hierarchical.txt"
     cmd = f"vsearch --sintax {nohit_ASVs} -db {db} -tabbedout {output} -threads {threads} -strand plus 2>> {log}"
-    conda_path = subprocess.check_output("conda info | grep 'base environment'", shell=True).decode("utf8").replace("base environment : ", "").replace("(read only)", "").strip()
+    conda_path = (
+        subprocess.check_output("conda info | grep 'base environment'", shell=True)
+        .decode("utf8")
+        .replace("base environment : ", "")
+        .replace("(read only)", "")
+        .strip()
+    )
     conda_act_path = conda_path + "/etc/profile.d/conda.sh"
-    subprocess.run(f". {conda_act_path} && conda activate {conda} && {cmd} && conda deactivate", shell=True)
+    subprocess.run(
+        f". {conda_act_path} && conda activate {conda} && {cmd} && conda deactivate",
+        shell=True,
+    )
     print("##### Finished Hierarchical Vsearch Classification #####")
 
 
@@ -201,7 +248,9 @@ def format_hierarchical_classification(output, threshold):
             if len(entry) > 0:
                 split = entry.rsplit("(", 1)
                 names.append(split[0])
-                values.append(split[1].replace(")", "").replace("\t", "").replace("+", ""))
+                values.append(
+                    split[1].replace(")", "").replace("\t", "").replace("+", "")
+                )
             else:
                 split = ""
                 names.append(split)
@@ -217,10 +266,18 @@ def format_hierarchical_classification(output, threshold):
                 names_lst[i][j] == ""
 
     names_df = pd.DataFrame(names_lst)
-    names_df.columns = ["Kingdom", "Phylum", "Class", "Order", "Family", "Genus", "Species"]
+    names_df.columns = [
+        "Kingdom",
+        "Phylum",
+        "Class",
+        "Order",
+        "Family",
+        "Genus",
+        "Species",
+    ]
     names_df.insert(0, "ASV", otu_name_lst)
     names_df["Kingdom"] = names_df.loc[:, "Kingdom"].str.replace("d:", "k:")
-    return(names_df)
+    return names_df
 
 
 def get_tax_from_samfile(samfile_path):
@@ -245,7 +302,7 @@ def get_tax_from_samfile(samfile_path):
     ranks_lst = ["Kingdom", "Phylum", "Class", "Order", "Family", "Genus", "Species"]
     if os.path.getsize(samfile_path) == 0:
         print(f"No hits in {samfile_path}.")
-        return(pd.DataFrame(columns=["ASV"] + ranks_lst))
+        return pd.DataFrame(columns=["ASV"] + ranks_lst)
 
     sam_df = pd.read_csv(samfile_path, sep="\t", header=None)
     sam_df = sam_df.iloc[:, [0, 2]]
@@ -253,7 +310,9 @@ def get_tax_from_samfile(samfile_path):
     tax_lst = sam_df.loc[:, 2].tolist()
     edited_tax_lst = []
     for tax in tax_lst:
-        tax_string = re.split(".;tax=", tax)[1]  # removes the identifier at the start of the taxonomy values
+        tax_string = re.split(".;tax=", tax)[
+            1
+        ]  # removes the identifier at the start of the taxonomy values
         tax_string = tax_string.removesuffix(";")
         edited_tax_lst.append(tax_string)
 
@@ -290,24 +349,32 @@ def get_tax_from_samfile(samfile_path):
 
     sam_df[ranks_lst] = tax_lst_of_lst_with_NA  # add taxonomy as seperate columns
 
-    unique_ASV_lst = list(set(sam_df["ASV"].tolist()))  # gets the name of all ASVs and removes duplicates
+    unique_ASV_lst = list(
+        set(sam_df["ASV"].tolist())
+    )  # gets the name of all ASVs and removes duplicates
     taxonomy_lst_of_lst = []
     for ASV in unique_ASV_lst:
-        ASV_df = (sam_df[sam_df.loc[:, "ASV"] == ASV])  # create new dataframe for every ASV
+        ASV_df = sam_df[
+            sam_df.loc[:, "ASV"] == ASV
+        ]  # create new dataframe for every ASV
         if len(ASV_df) == 1:
             tax_row = ASV_df.iloc[0].tolist()
             taxonomy_lst_of_lst.append(tax_row)
         else:
             tax_row = [ASV]
             for tax_rank in ranks_lst:
-                unique_tax_set = set(ASV_df.loc[:, tax_rank])  # removes duplicates from column
+                unique_tax_set = set(
+                    ASV_df.loc[:, tax_rank]
+                )  # removes duplicates from column
                 if len(unique_tax_set) == 1:
                     tax_row.append(list(unique_tax_set)[0])
                 else:
                     tax_row.append("")
             taxonomy_lst_of_lst.append(tax_row)
-    cleaned_taxonomy_table = pd.DataFrame(taxonomy_lst_of_lst, columns=(["ASV"] + ranks_lst))
-    return(cleaned_taxonomy_table)
+    cleaned_taxonomy_table = pd.DataFrame(
+        taxonomy_lst_of_lst, columns=(["ASV"] + ranks_lst)
+    )
+    return cleaned_taxonomy_table
 
 
 def statistics(log, stats_table_path):
@@ -346,14 +413,19 @@ def statistics(log, stats_table_path):
         number_of_hits_lst.append(integers_from_line[0])
         if i == 0:
             number_of_sequences = integers_from_line[1]
-    integers_from_line_hierarchical = [int(x) for x in hierarchical_matches_line.split() if x.isnumeric()]
+    integers_from_line_hierarchical = [
+        int(x) for x in hierarchical_matches_line.split() if x.isnumeric()
+    ]
 
     classification_type_lst = []
     for i in range(len(number_of_hits_lst)):
         classification_type_lst.append(f"Direct #{i+1}")
     classification_type_lst.append("Hierarchical")
     number_of_hits_lst.append(integers_from_line_hierarchical[0])
-    overall_percentage_lst = [f"{round((num_hits/number_of_sequences)*100, 2)}%" for num_hits in number_of_hits_lst]
+    overall_percentage_lst = [
+        f"{round((num_hits/number_of_sequences)*100, 2)}%"
+        for num_hits in number_of_hits_lst
+    ]
     sequences_left_lst = []
     num_seq = number_of_sequences
     for num_hits in number_of_hits_lst:
@@ -361,9 +433,18 @@ def statistics(log, stats_table_path):
         num_seq = num_seq - num_hits
     relative_percentage_lst = []
     for j in range(len(number_of_hits_lst)):
-        relative_percentage_lst.append(f"{round((number_of_hits_lst[j]/sequences_left_lst[j])*100, 2)}%")
+        relative_percentage_lst.append(
+            f"{round((number_of_hits_lst[j]/sequences_left_lst[j])*100, 2)}%"
+        )
 
-    stat_dict = {"Classification Type": classification_type_lst, "Database": database_lst, "Number of hits": number_of_hits_lst, "Out of": sequences_left_lst, "Relative Percentage": relative_percentage_lst, "Overall Percentage": overall_percentage_lst}
+    stat_dict = {
+        "Classification Type": classification_type_lst,
+        "Database": database_lst,
+        "Number of hits": number_of_hits_lst,
+        "Out of": sequences_left_lst,
+        "Relative Percentage": relative_percentage_lst,
+        "Overall Percentage": overall_percentage_lst,
+    }
     stat_df = pd.DataFrame(stat_dict)
     stat_df.index.name = "Run Nr."
     stat_df.to_csv(stats_table_path, sep="\t")
@@ -386,7 +467,16 @@ def main():
     nohit_path = ASVs
     for db in db_lst:
         count += 1
-        direct_classification(nohit_path, db, threshold, output, str(threads), str(count), conda_path, logfile)
+        direct_classification(
+            nohit_path,
+            db,
+            threshold,
+            output,
+            str(threads),
+            str(count),
+            conda_path,
+            logfile,
+        )
         samfile_path = f"{os.path.splitext(output)[0]}.{count}.direct.sam"
         tax_from_samfile_df = get_tax_from_samfile(samfile_path)
         nohit_path, hit_ASVs = format_dir_classification(nohit_path, output, str(count))
@@ -394,19 +484,50 @@ def main():
             taxonomy_sans_multihits = tax_from_samfile_df
             hits_df = hit_ASVs
         else:
-            taxonomy_sans_multihits = taxonomy_sans_multihits.append(tax_from_samfile_df)
+            taxonomy_sans_multihits = taxonomy_sans_multihits.append(
+                tax_from_samfile_df
+            )
             hits_df = hits_df.append(hit_ASVs)
-        hits_df.to_csv(os.path.splitext(output)[0] + ".direct.full.txt", sep="\t", header=None, index=None)
+        hits_df.to_csv(
+            os.path.splitext(output)[0] + ".direct.full.txt",
+            sep="\t",
+            header=None,
+            index=None,
+        )
     hierarchical_classification(nohit_path, h_db, output, threads, conda_path, logfile)
-    formatted_hierarchical_classification = format_hierarchical_classification(output, sintax_cutoff_hierarchical)
+    formatted_hierarchical_classification = format_hierarchical_classification(
+        output, sintax_cutoff_hierarchical
+    )
     if len(hits_df) > 0:
         formatted_direct_classification = taxonomy_sans_multihits
-        taxonomy_df = formatted_direct_classification.append(formatted_hierarchical_classification, ignore_index=True)
+        taxonomy_df = formatted_direct_classification.append(
+            formatted_hierarchical_classification, ignore_index=True
+        )
     else:
         taxonomy_df = formatted_hierarchical_classification
+
+    """
+    At the moment, the taxonomy_df dataframe contains cells with the string "", but some sc
+    ripts cant work with that, thats why we replace it in the next few lines
+    """
+    columns_to_fill = [
+        "Kingdom",
+        "Phylum",
+        "Class",
+        "Order",
+        "Family",
+        "Genus",
+        "Species",
+    ]
+    replace_values = ["k:", "p:", "c:", "o:", "f:", "g:", "s:"]
+    for column, value in zip(columns_to_fill, replace_values):
+        taxonomy_df[column] = taxonomy_df[column].replace("", np.nan).fillna(value)
+
     taxonomy_df.to_csv(output, sep="\t")
     tax_df_for_krona = taxonomy_df.drop(["ASV"], axis=1)
-    tax_df_for_krona.to_csv(os.path.splitext(output)[0] + ".krona.txt", sep="\t", header=False, index=False)
+    tax_df_for_krona.to_csv(
+        os.path.splitext(output)[0] + ".krona.txt", sep="\t", header=False, index=False
+    )
 
     statistics_file_path = f"{os.path.dirname(output)}/stats_mqc.csv"
     statistics(logfile, statistics_file_path)
